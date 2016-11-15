eebb170ea78d8b5cf050d47be9ea65a6f2b0888e
Rebelpebble <neasbrian@gmail.com>
2016-11-15 00:11:53 -0600

    Tweak format for torque output

--- torsion.py
+++ torsion.py
@@ -20,6 +20,6 @@ shear_yield_strength = shear_ultimate_strength * YIELD_REDUCTION_FACTOR
 reserve_factor = shear_yield_strength / shear_stress
 
 print "Mean Area = %.2f in^2" % mean_area
-print "Torque = %.2f in*lbs" % torque
+print "Torque = %.0f in*lbs" % torque
 print "Shear Stress = %.0f psi" % shear_stress
 print "Reserve Factor = %.2f" % reserve_factor
